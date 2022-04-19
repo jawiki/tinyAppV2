84491430bdd3dfdb3ae2309c94858715dff4a33f
JarrettK <jkirck@hotmaill.com>
2022-04-18 20:36:29 -0400

    updates

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 6060; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
