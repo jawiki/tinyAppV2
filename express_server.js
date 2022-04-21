1da8199bbb20eb06dcdd7fd2f3f026959e73dc02
JarrettK <jkirck@hotmaill.com>
2022-04-21 19:38:08 -0400

    more scripts

--- express_server.js
+++ express_server.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 6060; // default port 8080
+const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
+const cookieSession = require("cookie-session");
+const {generateRandomString, searchUserByEmail}  = require('./helpers');
+const req = require("express/lib/request");
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ name:"session", keys:["hello", "world"]}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
 app.listen(PORT, () => {
